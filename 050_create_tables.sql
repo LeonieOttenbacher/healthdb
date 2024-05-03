1d140d903a04635acd4027446eb49910ca30d5c8
PMWNTMAIL\OttenbacheL <Leonie.Ottenbacher@putzmeister.com>
2024-05-03 19:05:13 +0200

    letzter Stand vor Umbau auf app.py

--- 050_create_tables.sql
+++ 050_create_tables.sql
@@ -1,55 +1,55 @@
-# Land-Tabelle
+# Tabelle für Laender
 
 CREATE TABLE IF NOT EXISTS Laender (
-    Land_ID INT AUTO_INCREMENT PRIMARY KEY,
-    CC CHAR(2),
-    Name VARCHAR(100)
+    Land_ID INT AUTO_INCREMENT PRIMARY KEY, -- ID für jedes Land
+    CC CHAR(2), -- Ländercode
+    Name VARCHAR(100) -- Name des Landes
 );
 
-# Probanden-Tabelle
+# Tabelle für Probanden
 CREATE TABLE IF NOT EXISTS Probanden(
     Probanden_ID INT AUTO_INCREMENT PRIMARY KEY,
     Vorname VARCHAR(50),
     Nachname VARCHAR(50),
     Email VARCHAR(100),
-    Geschlecht ENUM('M','F','D'),
+    Geschlecht ENUM('M','F','D'), -- Geschlecht des Probanden (männlich, weiblich, divers)
     Geburtstag DATE,
-    KGewicht DECIMAL(5,2),
-    KGroesse DECIMAL(5,2),
-    Land_ID INT,
-    updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    KGewicht DECIMAL(5,2),  -- Körpergewicht des Probanden
+    KGroesse DECIMAL(5,2), -- Körpergröße des Probanden
+    Land_ID INT, -- Verweis auf das Herkunftsland des Probanden
+    updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- für Aktualisierungen
     FOREIGN KEY (Land_ID) REFERENCES Laender(Land_ID)
 );
 
 
- # Krankheiten-Tabelle
+ # Tabelle für Krankheiten
 CREATE TABLE IF NOT EXISTS Krankheiten(
-    Krankheits_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Krankheits_ID INT AUTO_INCREMENT PRIMARY KEY, -- ID für jede Krankheit
     Name VARCHAR(100)
 );
 
-# Medikamente-Tabelle
+# Tabelle für Medikamente
 CREATE TABLE IF NOT EXISTS Medikamente(
-    Medikamenten_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Medikamenten_ID INT AUTO_INCREMENT PRIMARY KEY, -- ID für jedes Medikament
     Name VARCHAR(100)
 );
 
-# Verknüpfung Probanden-Krankheiten
+# Verknüpfungstabelle für Probanden-Krankheiten
 CREATE TABLE  Probanden_Krankheiten(
-    Probanden_ID INT,
-    Krankheits_ID INT,
+    Probanden_ID INT, -- Verweis auf den Probanden
+    Krankheits_ID INT, -- Verweis auf die Krankheit
     PRIMARY KEY (Probanden_ID,Krankheits_ID),
-    FOREIGN KEY (Probanden_ID) REFERENCES Probanden(Probanden_ID),
-    FOREIGN KEY (Krankheits_ID) REFERENCES Krankheiten(Krankheits_ID)
+    FOREIGN KEY (Probanden_ID) REFERENCES Probanden(Probanden_ID), -- Fremdschlüsselbeziehung zur Probanden-Tabelle
+    FOREIGN KEY (Krankheits_ID) REFERENCES Krankheiten(Krankheits_ID) -- Fremdschlüsselbeziehung zur Krankheiten-Tabelle
 );
 
-# Verknüpfung Probanden-Medikamente
+# Verknüpfungstabelle Probanden-Medikamente
 CREATE TABLE Probanden_Medikamente(
     Probanden_ID INT,
     Medikamente_ID INT,
     PRIMARY KEY (Probanden_ID,Medikamente_ID),
-    FOREIGN KEY (Probanden_ID) REFERENCES Probanden(Probanden_ID),
-    FOREIGN KEY (Medikamente_ID) REFERENCES Medikamente(Medikamenten_ID)
+    FOREIGN KEY (Probanden_ID) REFERENCES Probanden(Probanden_ID), -- Fremdschlüsselbeziehung zur Probanden-Tabelle
+    FOREIGN KEY (Medikamente_ID) REFERENCES Medikamente(Medikamenten_ID) -- Fremdschlüsselbeziehung zur Medikamente-Tabelle
 );
 
 
